31937f13e8340541bc48d817e2687d8b9af7ad6c
Jose Cambronero <jcamsan@mit.edu>
2019-07-21 15:06:41 -0400

    added fake data generator

---
+++ generate_ts.py
@@ -0,0 +1,101 @@
+from argparse import ArgumentParser
+
+import pandas as pd
+from rpy2 import robjects
+from rpy2.robjects import pandas2ri
+from rpy2.robjects.packages import importr
+
+pandas2ri.activate()
+rinterpreter = robjects.r
+rinterpreter.source("timeseries.R")
+
+generate_dataset_ = rinterpreter("generate_dataset")
+
+
+def generate_dataset(
+        num_ts,
+        num_obs,
+        probs=None,
+        methods=None,
+        num_iters=10,
+        seed=42,
+):
+    if probs is None:
+        probs = [0.2]
+    if methods is None:
+        methods = ["mean"]
+    df = generate_dataset_(
+        num_ts,
+        num_obs,
+        probs=probs,
+        methods=methods,
+        num_iters=num_iters,
+        seed=seed,
+    )
+    return df
+
+
+def get_args():
+    parser = ArgumentParser(
+        description="Generate dataset for timeseries w/ missing data")
+    parser.add_argument(
+        "--output",
+        type=str,
+        help="Location to save down dataframe",
+    )
+    parser.add_argument(
+        "--num_ts",
+        type=int,
+        help="Number of different original (complete) timeseries",
+    )
+    parser.add_argument(
+        "--num_obs",
+        type=int,
+        help="Number of observations per time series",
+    )
+    parser.add_argument(
+        "--probs",
+        type=float,
+        nargs="+",
+        help=
+        "Missing values with given probabilities (generates new missing mask per prob)",
+    )
+    parser.add_argument(
+        "--num_iters",
+        type=int,
+        help="Number of times to generate each missing prob mask",
+    )
+    parser.add_argument(
+        "--methods",
+        type=str,
+        nargs="+",
+        help="List of imputation methods to try (see timeseries.R for details)"
+    )
+    parser.add_argument(
+        "--seed",
+        type=int,
+        help="RNG seed",
+        default=42,
+    )
+    return parser.parse_args()
+
+
+def main():
+    args = get_args()
+    df = generate_dataset(
+        args.num_ts,
+        args.num_obs,
+        probs=args.probs,
+        methods=args.methods,
+        num_iters=args.num_iters,
+        seed=args.seed,
+    )
+    df.to_csv(args.output, index=False)
+
+
+if __name__ == "__main__":
+    try:
+        main()
+    except Exception as err:
+        import pdb
+        pdb.post_mortem()
---
+++ timeseries.R
@@ -0,0 +1,91 @@
+require("imputeTS")
+require("progress")
+
+gen_ts_config <- function(max_ar=5, max_ma=5, max_diff=3) {
+  num_ar <- sample(1:max_ar, 1)
+  num_ma <- sample(1:max_ma, 1)
+  num_d <- sample(1:max_diff, 1)
+
+  frac_non_ar <- runif(1, 0.0, 1.0)
+  ar <- runif(num_ar, 0.0, 1.0)
+  # dumb way to make sure stationary
+  ar <- (ar / sum(ar))  * (1 - frac_non_ar)
+  ma <- runif(num_ma, 0.0, 1.0)
+
+  list(order=c(num_ar, num_d, num_ma), ar=ar, ma=ma)
+}
+
+
+gen_ts <- function(config, n) {
+  vec <- arima.sim(model=config, n=n)
+  vec <- as.numeric(vec)
+  vec[seq(config$order[3], n)]
+}
+
+
+gen_missing_mask <- function(n, prob) {
+    runif(n) < prob
+}
+
+
+add_missing <- function(vec, prob) {
+    mask <- gen_missing_mask(length(vec), prob)
+    copy_vec <- vec
+    copy_vec[mask] <- NA
+    list(orig=vec, with_missing=copy_vec, mask=mask)
+}
+
+impute_missing <- function(vec, method_name) {
+    methods <- list(
+      mean=function(x) na_mean(x, option="mean"),
+      median=function(x) na_mean(x, option="median"),
+      mode=function(x) na_mean(x, option="mode"),
+      random=function(x) na_random(x),
+      forward=function(x) na_locf(x, option="locf"),
+      backward=function(x) na_locf(x, option="nocb")
+    )
+    chosen_method <- methods[[method_name]]
+    chosen_method(vec)
+}
+
+generate_dataset <- function(num_ts, num_obs=200, probs=c(0.2), methods=c("mean"), num_iters=10, seed=NULL) {
+    if (!is.null(seed)) {
+      set.seed(seed)
+    }
+
+    all_generated <- list()
+    list_ix <- 1
+
+    total_iters <- num_ts * length(probs) * num_iters * length(methods)
+    pb_bar <- progress_bar$new(total=total_iters)
+
+
+    for (ts_id in seq(num_ts)) {
+        ts_config <- gen_ts_config()
+        ts_vec <- gen_ts(ts_config, num_obs)
+
+        for (prob in probs) {
+
+          for (iter_ in seq(num_iters)) {
+
+              modified <- add_missing(ts_vec, prob)
+              for (method in methods) {
+
+                copied <- modified
+                copied$filled <- impute_missing(copied$with_missing, method)
+
+                generated_df <- as.data.frame(copied)
+                generated_df$prob <- prob
+                generated_df$method <- method
+                generated_df$ts_id <- ts_id
+                generated_df$iter <- iter_
+
+                all_generated[[list_ix]] <- generated_df
+                list_ix <- list_ix + 1
+                pb_bar$tick()
+              }
+          }
+        }
+    }
+    do.call(rbind, all_generated)
+}
