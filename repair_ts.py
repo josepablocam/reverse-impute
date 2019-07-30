e9d87dcbc0fb4d643655c9cbe0e2034831b41ea5
Jose Cambronero <jcamsan@mit.edu>
2019-07-30 14:37:32 -0400

    fix greedy

--- repair_ts.py
+++ repair_ts.py
@@ -6,6 +6,7 @@ from rpy2.robjects import pandas2ri, numpy2ri
 from rpy2.robjects.packages import importr
 import numpy as np
 import torch
+import torch.nn as nn
 import tqdm
 
 pandas2ri.activate()
@@ -16,32 +17,34 @@ impute_missing_ = rinterpreter("impute_missing")
 minimize_mse_ = rinterpreter("minimize_mse")
 
 
-class GreedyMSEMinimizer(object):
+class GreedyMSEMinimizer(nn.Module):
     def __init__(self, model):
+        super().__init__()
         self.model = model.eval()
 
     def predict_is_imputed(self, X, step_size, extra_info=False):
-        probs = self.model.probability_is_imputed(X)
-        probs = probs.numpy()
-        acc = []
-        for i in tqdm.tqdm(range(0, probs.shape[0])):
-            v = pd.Series(X[i, :])
-            p = pd.Series(probs[i, :])
-            result = minimize_mse_(v, p, step_size)
-            result = {k: numpy2ri.ri2py(v) for v in result.items()}
-            result = {k: v[0] if len(v) == 1 else v for k, v in result.items()}
-            acc.append(result)
-        df = pd.DataFrame(acc)
-        yhat = np.array([e for e in df.predicted.values])
-        if extra_info:
-            return yhat, df
-        else:
-            return yhat
+        with torch.no_grad():
+            probs = self.model.probability_is_imputed(X)
+            probs = probs.numpy()
+            acc = []
+            for i in tqdm.tqdm(range(0, probs.shape[0])):
+                v = pd.Series(X[i, :])
+                p = pd.Series(probs[i, :])
+                result = minimize_mse_(v, p, step_size)
+                result = {k: numpy2ri.ri2py(v) for v in result.items()}
+                result = {k: v[0] if len(v) == 1 else v for k, v in result.items()}
+                acc.append(result)
+            df = pd.DataFrame(acc)
+            yhat = np.array([e for e in df.predicted.values])
+            if extra_info:
+                return yhat, df
+            else:
+                return yhat
 
     def probability_is_imputed(self, X):
         # prob == 1.0 if we predict it, just for easy evaluation
-        df = self.predict_is_imputed(self, X)
-        return df.predicted.values.astype(float)
+        yhat = self.predict_is_imputed(self, X, extra_info=False)
+        return yhat.astype(float)
 
 
 class RepairImpute(object):
